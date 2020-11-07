bcc778eb933f3fbcfd1e9ef384e7b8ac7faa5204
GavinCoupDigipen <53310510+GavinCoupDigipen@users.noreply.github.com>
2020-11-06 18:13:19 -0800

    Added Delay to ChangeScene
    Added a delay so you dont instantly go to the next scene.

---
+++ Assets/Scripts/EventChangeScene.cs
@@ -0,0 +1,22 @@
+﻿////////////////////////////
+/// By: Gavin C
+/// Date: 11/6/2020
+/// Desription: This is a script to be accused via unity events to change the scene
+///////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EventChangeScene : MonoBehaviour
+{
+    [Tooltip("Name of the scene you want this script to load")]
+    public string SceneName;
+
+    //Load new scene
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
